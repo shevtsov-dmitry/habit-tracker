3766b2fd48aa737a9faabcf764dadf3c749186f8
shevtsov-dmitry <shadowstakeus28@gmail.com>
2025-07-29 19:23:34 +0300

    refactor: tauri client+ quarkus backend

---
+++ client/src/components/App.tsx
@@ -0,0 +1,11 @@
+import "../App.css";
+
+function App() {
+  return (
+    <main className="container">
+      <h1 className="bg-red-500 px-10 py-10 font-bold text-white">App</h1>
+    </main>
+  );
+}
+
+export default App;
