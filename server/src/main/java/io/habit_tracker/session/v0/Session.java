6338786d5b9ffedd24bfe945e7ec980d3e196643
shevtsov-dmitry <shadowstakeus28@gmail.com>
2025-07-30 12:02:02 +0300

    feat(project): lombok for entities

--- server/src/main/java/io/habit_tracker/session/v0/Session.java
+++ server/src/main/java/io/habit_tracker/session/v0/Session.java
@@ -2,10 +2,18 @@ package io.habit_tracker.session.v0;
 
 import io.habit_tracker.user.v0.User;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class Session {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
--- server/src/main/java/io/habit_tracker/user/v0/User.java
+++ server/src/main/java/io/habit_tracker/user/v0/User.java
@@ -2,10 +2,18 @@ package io.habit_tracker.user.v0;
 
 import io.habit_tracker.session.v0.Session;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class User {
 
   @Id
