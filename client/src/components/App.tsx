de9963493ffb2f43cf61a2113a24d378feba6db5
shevtsov-dmitry <shadowstakeus28@gmail.com>
2025-08-01 16:50:23 +0300

    feat(style): decide with back graident color of app at light mode

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,17 +1,22 @@
 import "../App.css";
-import '../i18n/i18n.js';
+import "../i18n/i18n.js";
 
 import SessionManagement from "./session_management/SessionManagement.tsx";
 import TopPanel from "./top_panel/TopPanel.tsx";
 
 function App() {
-
-    return (
-        <main className="min-w-screen min-h-screen bg-pink-100 dark:bg-cyan-700">
-            <TopPanel/>
-            <SessionManagement/>
-        </main>
-    );
+  return (
+    <main
+      className="min-w-screen min-h-screen dark:bg-cyan-700"
+      style={{
+        backgroundImage:
+          "linear-gradient(to left bottom, #eee7e9, #f0ebed, #f2eff1, #f4f2f4, #f7f6f7, #f6f5f6, #f6f5f6, #f5f4f5, #f1eff0, #eee9eb, #eae4e5, #e7dfdf)",
+      }}
+    >
+      <TopPanel />
+      <SessionManagement />
+    </main>
+  );
 }
 
 export default App;
