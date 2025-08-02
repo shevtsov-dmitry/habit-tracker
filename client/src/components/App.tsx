d5addf5e7750f7571c7b362282b389bf987f2ade
shevtsov-dmitry <shadowstakeus28@gmail.com>
2025-08-02 08:03:49 +0300

    feat(project): use file with constant variables in client

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -7,7 +7,7 @@ import TopPanel from "./top_panel/TopPanel.tsx";
 function App() {
   return (
     <main
-      className="min-w-screen min-h-screen dark:bg-cyan-700"
+      className="min-w-screen min-h-screen "
       style={{
         backgroundImage:
           "linear-gradient(to left bottom, #eee7e9, #f0ebed, #f2eff1, #f4f2f4, #f7f6f7, #f6f5f6, #f6f5f6, #f5f4f5, #f1eff0, #eee9eb, #eae4e5, #e7dfdf)",
--- client/src/components/session_management/SessionManagement.tsx
+++ client/src/components/session_management/SessionManagement.tsx
@@ -1,45 +1,125 @@
-import {useTranslation} from "react-i18next";
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { SERVER_ENDPOINT_URL } from "../../constants.js";
 
 const SessionManagement = () => {
+  const { t } = useTranslation();
 
-    const {t} = useTranslation()
+  enum SessionStatus {
+    ACTIVE,
+    INACTIVE,
+    SUSPENDED,
+  }
+  
+  interface OperationConfirmer {
+    operationType: SessionStatus | null,
+    confirmed: Boolean
+  }
 
-    const SessionStatus = () => {
-        return <>
-            <p>Current session progress</p>
-            <p>log: your session still running...</p>
-        </>;
+  const [sessionTime, setSessionTime] = useState<number>(0);
+  const [sessionStatus, setSessionStatus] = useState<SessionStatus>(
+    SessionStatus.INACTIVE
+  );
+
+  const [operationConfirmer, setOperationConfirmer] = useState<OperationConfirmer>()
+
+  function startSession() {
+    setSessionStatus(SessionStatus.ACTIVE);
+    // fetch(SERVER_ENDPOINT_URL + "/api/v0/sessions/start")
+  }
+
+  function pauseSession() {
+    setSessionStatus(SessionStatus.SUSPENDED);
+    // fetch(SERVER_ENDPOINT_URL + "/api/v0/sessions/pause")
+  }
+
+  function stopSession() {
+    setSessionStatus(SessionStatus.INACTIVE);
+    // fetch(SERVER_ENDPOINT_URL + "/api/v0/sessions/end")
+    resetSession();
+  }
+
+  function resetSession() {
+    setSessionStatus(SessionStatus.INACTIVE);
+    setSessionTime(0);
+  }
+
+
+  useEffect(() => {
+    let interval: ReturnType<typeof setInterval> | null = null;
+
+    if (sessionStatus === SessionStatus.ACTIVE) {
+      interval = setInterval(() => {
+        setSessionTime((prevVal) => prevVal + 1);
+      }, 1000);
     }
 
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    };
+  }, [sessionStatus]);
 
+  const SessionStatusCounter = () => {
+    return (
+      <>
+        <p className="font-mono font-bold text-6xl w-full text-center">
+          {sessionTime}
+        </p>
+      </>
+    );
+  };
 
-    const OperationButtons = () => {
+  // const ConfirmationWindow = 
+  
 
-        const style = {
-            buttons: "w-32 h-16 shadow-md rounded-2xl hover:border-2 hover:border-zinc-400",
-            buttons_text: "font-bold text-white"
-        }
-
-        return <div className={"flex gap-3"}>
-            <button className={style.buttons + " bg-green-300 dark:bg-green-600 "}>
-                <p className={style.buttons_text}>
-                    {t('session_management.start')}
-                </p>
-            </button>
-            <button className={style.buttons + " bg-red-300 dark:bg-red-700"}>
-                <p className={style.buttons_text}>
-                    {t('session_management.stop')}
-                </p>
-            </button>
-        </div>;
-    }
+  const OperationButtons = () => {
+    const style = {
+      buttons:
+        "w-16 h-16 shadow-md rounded-2xl hover:border-2 hover:border-zinc-400 hover:cursor-pointer",
+      buttons_text: "font-bold text-black dark:text-white text-3xl select-none",
+    };
 
     return (
-        <div className="w-fit h-fit p-8 rounded-4xl shadow-2xl bg-neutral-50 m-auto">
-            <SessionStatus/>
-            <OperationButtons/>
-        </div>
+      <div className={"flex gap-3"}>
+        <button
+          className={style.buttons + " bg-green-300 dark:bg-green-600"}
+          onClick={startSession}
+        >
+          <p className={style.buttons_text + " mr-[-5px]  mt-[-5px]"}>▶</p>
+        </button>
+        <button
+          className={style.buttons + " bg-yellow-300 dark:bg-yellow-500"}
+          onClick={pauseSession}
+        >
+          <p className={style.buttons_text + " mb-[-2px]"}>II</p>
+        </button>
+        <button
+          className={style.buttons + " bg-red-300 dark:bg-red-700"}
+          onClick={stopSession}
+        >
+          <p className={style.buttons_text + "  mt-[-5px]"}>◼</p>
+        </button>
+        <button
+          className={style.buttons + " bg-blue-300 dark:bg-blue-700"}
+          onClick={resetSession}
+        >
+          <p className={style.buttons_text + "  mt-[-5px]"}>⎌</p>
+        </button>
+      </div>
     );
-}
+  };
+
+  return (
+    <div className="w-fit h-fit p-8 rounded-4xl shadow-2xl bg-neutral-50 m-auto">
+      <h1 className="font-bold text-2xl text-center w-full">Session status</h1>
+      <div className="my-2" />
+      <SessionStatusCounter />
+      <div className="my-2" />
+      <OperationButtons />
+    </div>
+  );
+};
 
 export default SessionManagement;
