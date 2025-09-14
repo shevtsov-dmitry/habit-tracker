7b49f20fefdeb83d8cb92d014e677b393dc1e49e
shevtsov-dmitry <shadowstakeus28@gmail.com>
2025-09-14 21:34:53 +0300

    feat: add gradient dark theme background

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,11 +1,21 @@
+import { ReactElement } from "react";
 import "../App.css";
+import TopPanel from "./top-panel/TopPanel";
 
-function App() {
+const App = (): ReactElement => {
   return (
-    <main className="container">
-      <h1 className="bg-red-500 px-10 py-10 font-bold text-white">App</h1>
+    <main
+      className="flex min-h-screen w-full flex-col items-center bg-gray-50 text-gray-900"
+      style={{
+        backgroundImage:
+          "linear-gradient(to right top, #181d18, #171f1c, #162120, #172224, #1a2328, #1b2429, #1d242a, #1e252b, #1d262a, #1d2629, #1e2728, #1f2727)",
+      }}
+    >
+      <div className="flex w-full max-w-5xl flex-1 flex-col overflow-y-auto p-6">
+        <TopPanel />
+      </div>
     </main>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/top-panel/TopPanel.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from "react";
+
+const TopPanel = (): ReactElement => {
+  return <div className="flex"></div>;
+};
+
+export default TopPanel;
