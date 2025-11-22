811d60949b2e988011cec66992274eb3ae4179fa
shevtsov-dmitry <shadowstakeus28@gmail.com>
2025-11-22 16:48:35 +0300

    feat: implement business  logic of web sockets for active timer

--- server/src/main/java/app/habit_tracker/backend/session/SessionController.java
+++ server/src/main/java/app/habit_tracker/backend/session/SessionController.java
@@ -1,4 +1,16 @@
 package app.habit_tracker.backend.session;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class SessionController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/messages")
+    public String hello() {
+        return "Server says: Hello World!";
+    }
+
 }
